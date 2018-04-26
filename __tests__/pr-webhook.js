04bcdc6ca46ee560cec566ca299a8ca1e9996b27
Domenic Denicola <d@domenic.me>
2018-04-26 15:12:31 -0400

    Check that PR exists before posting a status
    This should prevent issues like https://github.com/whatwg/html/issues/3582 going forward, and was suggested by GitHub support.

--- __tests__/pr-webhook.js
+++ __tests__/pr-webhook.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const fromBranch = require("./__fixtures__/pr-from-branch-hook-payload.json");
 const fromFork = require("./__fixtures__/pr-from-fork-hook-payload.json");
 
@@ -13,12 +12,18 @@ jest.mock("../lib/get-user-status.js", () => {
 });
 
 let mockCreateStatus;
+let mockPRGet;
 jest.mock("../lib/helpers/github.js", () => {
   return {
     api: {
       repos: {
         get createStatus() {
           return mockCreateStatus;
+        }
+      },
+      pullRequests: {
+        get get() {
+          return mockPRGet;
         }
       }
     }
@@ -29,9 +34,12 @@ const prWebhook = require("../lib/pr-webhook.js");
 
 beforeEach(() => {
   mockCreateStatus = jest.fn();
+  mockPRGet = jest.fn();
 });
 
 test("A PR from a branch must trigger an appropriate status update", async () => {
+  mockPRGet.mockReturnValue(Promise.resolve());
+
   await prWebhook(fromBranch);
 
   expect(mockCreateStatus).toHaveBeenCalledTimes(1);
@@ -39,8 +47,38 @@ test("A PR from a branch must trigger an appropriate status update", async () =>
 });
 
 test("A PR from a fork must trigger an appropriate status update", async () => {
+  mockPRGet.mockReturnValue(Promise.resolve());
+
   await prWebhook(fromFork);
 
   expect(mockCreateStatus).toHaveBeenCalledTimes(1);
   expect(mockCreateStatus.mock.calls[0]).toMatchSnapshot();
 });
+
+test("A PR that no longer exists from a fork must trigger no status update", async () => {
+  // eslint-disable-next-line prefer-promise-reject-errors
+  mockPRGet.mockReturnValue(Promise.reject({ code: 404 }));
+
+  await prWebhook(fromFork);
+
+  expect(mockCreateStatus).toHaveBeenCalledTimes(0);
+});
+
+test("A PR that no longer exists from a branch must trigger no status update", async () => {
+  // eslint-disable-next-line prefer-promise-reject-errors
+  mockPRGet.mockReturnValue(Promise.reject({ code: 404 }));
+
+  await prWebhook(fromBranch);
+
+  expect(mockCreateStatus).toHaveBeenCalledTimes(0);
+});
+
+test("Non-404 errors getting the PR must be propagated", () => {
+  const notA404 = new Error("Internal error");
+  mockPRGet.mockReturnValue(Promise.reject(notA404));
+
+  expect(prWebhook(fromFork)).rejects.toThrow(notA404);
+
+  expect(mockCreateStatus).toHaveBeenCalledTimes(0);
+});
+
