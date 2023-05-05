0d5152a4012bbfc1574d60270c849f4bda3d3eae
Domenic Denicola <d@domenic.me>
2023-05-05 17:53:01 +0900

    Use Twitter API v2
    Closes https://github.com/whatwg/meta/issues/274.

--- __tests__/tweet-webhook.js
+++ __tests__/tweet-webhook.js
@@ -3,8 +3,8 @@ const pushToMain = require("./__fixtures__/push-to-main.json");
 const pushToMainMeta = require("./__fixtures__/push-to-main-meta.json");
 const pushToBranch = require("./__fixtures__/push-to-branch.json");
 
-jest.mock("twitter-lite");
-const Twitter = require("twitter-lite");
+jest.mock("twitter-api-v2");
+const { TwitterApi } = require("twitter-api-v2");
 
 jest.mock("../private-config.json", () => {
   return {
@@ -31,39 +31,38 @@ jest.mock("../private-config.json", () => {
 
 const tweetWebhook = require("../lib/tweet-webhook.js");
 
+let mockTweet;
 beforeEach(() => {
-  Twitter.mockClear();
+  mockTweet = jest.fn();
+  TwitterApi.mockClear().mockReturnValueOnce({ readWrite: { v2: { tweet: mockTweet } } });
 });
 
 test("A push to main with a substantive commit must trigger a tweet", async () => {
   await tweetWebhook(pushToMain);
 
-  expect(Twitter).toHaveBeenCalledTimes(1);
-  expect(Twitter).toHaveBeenCalledWith({
-    consumer_key: "appkey",
-    consumer_secret: "appsecret",
-    access_token_key: "domstandardkey",
-    access_token_secret: "domstandardsecret"
+  expect(TwitterApi).toHaveBeenCalledTimes(1);
+  expect(TwitterApi).toHaveBeenCalledWith({
+    appKey: "appkey",
+    appSecret: "appsecret",
+    accessToken: "domstandardkey",
+    accessSecret: "domstandardsecret"
   });
 
-  expect(Twitter.mock.instances[0].post).toHaveBeenCalledWith(
-    "statuses/update",
-    {
-      status: "Use a single exception for name validation (follow-up)\nhttps://github.com/whatwg/dom/commit/c80cbf52b351609b3a107bb2d047d6377ffafbf9"
-    }
+  expect(mockTweet).toHaveBeenCalledWith(
+    "Use a single exception for name validation (follow-up)\nhttps://github.com/whatwg/dom/commit/c80cbf52b351609b3a107bb2d047d6377ffafbf9"
   );
 });
 
 test("A push to main with a Meta: commit must not trigger a tweet", async () => {
   await tweetWebhook(pushToMainMeta);
 
-  expect(Twitter).not.toHaveBeenCalled();
+  expect(TwitterApi).not.toHaveBeenCalled();
 });
 
 test("A push to a non-main branch must not trigger a tweet", async () => {
   await tweetWebhook(pushToBranch);
 
-  expect(Twitter).not.toHaveBeenCalled();
+  expect(TwitterApi).not.toHaveBeenCalled();
 });
 
 // Tests to write (easier once we collect some events to paste into __fixtures__):
