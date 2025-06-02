98598efe1e9e6a303c009ec9d205d5e9f7f18648
Domenic Denicola <d@domenic.me>
2025-06-02 14:36:14 +0900

    Fix mocking strategy for private-config on CI

--- __tests__/__setup__.mjs
+++ __tests__/__setup__.mjs
@@ -1,4 +1,5 @@
 import * as path from "node:path";
+import * as fs from "node:fs";
 import { snapshot } from "node:test";
 
 snapshot.setResolveSnapshotPath(testFilePath => {
@@ -6,3 +7,15 @@ snapshot.setResolveSnapshotPath(testFilePath => {
   const filename = path.basename(testFilePath);
   return path.resolve(base, "__snapshots__", `${filename}.snap`);
 });
+
+// If there's no private-config.json (e.g., as there is on CI), copy the private-config.sample.json there.
+// This is especially necessary since Node.js's module mocking doesn't work on files that don't exist.
+const privateConfigPath = path.resolve(import.meta.dirname, "../private-config.json");
+const privateConfigSamplePath = path.resolve(import.meta.dirname, "../private-config.sample.json");
+try {
+  fs.copyFileSync(privateConfigSamplePath, privateConfigPath, fs.constants.COPYFILE_EXCL);
+} catch (e) {
+  if (e.code !== "EEXIST") {
+    throw e;
+  }
+}
